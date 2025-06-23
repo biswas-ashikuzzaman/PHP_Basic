e2034195e048d3daec3441698ada4dd58cb745de
hiclassic <biswasashikuzzaman@gmail.com>
2025-06-23 10:17:04 +0600

    update

---
+++ function.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP_Function</title>
+</head>
+<body>
+    <?php
+function greet() {
+    echo "Hello, Welcome to PHP!<br>";
+}
+
+greet();  // ফাংশন কল
+greet();  // আবার কল
+?>
+</body>
+</html>
---
+++ operator.php
@@ -0,0 +1,10 @@
+<?php
+$a = 10;
+$b = 3;
+
+echo "Addition: " . ($a + $b) . "<br>";       // 13
+echo "Subtraction: " . ($a - $b) . "<br>";    // 7
+echo "Multiplication: " . ($a * $b) . "<br>"; // 30
+echo "Division: " . ($a / $b) . "<br>";       // 3.333...
+echo "Modulus: " . ($a % $b) . "<br>";        // 1
+?>
