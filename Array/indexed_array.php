667b7d943d12b05fd38dcc9bb915eb7a29c35bee
hiclassic <biswasashikuzzaman@gmail.com>
2025-06-25 09:52:23 +0600

    update

---
+++ Array/indexed_array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indexed Array</title>
+</head>
+<body>
+    <?php 
+    $fruits= array("banna","mango","litchi","jackfruit","","","",);
+    echo $fruits[2]; //output
+    ?>
+    <!-- shortcut way -->
+     <?php
+    $fruits1= ["banna","mango","litchi","jackfruit","","","",];
+    echo $fruits1[3];
+     
+     ?>
+</body>
+</html>
