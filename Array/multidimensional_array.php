dcce2c9ac334beef6e82334306088794da3b54ae
hiclassic <biswasashikuzzaman@gmail.com>
2025-06-25 10:17:50 +0600

    update

---
+++ Array/multidimensional_array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Associative Array</title>
+</head>
+<body>
+    <?php
+    $student=array(
+        array("ashik",1288603,"jashore"),
+        array("lima",1230,"italy"),
+        array("rafi",0,"dhaka"),
+    );
+    echo$student[0][2];
+    ?>
+</body>
+</html>
