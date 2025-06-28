b4ef1ac1dabfcf94c9fba8eb9763259464956a01
hiclassic <biswasashikuzzaman@gmail.com>
2025-06-28 09:41:20 +0600

    update

--- Loop statement/positive-negative number.php
+++ Loop statement/positive-negative number.php
@@ -8,6 +8,30 @@
 <body>
     <?php 
     
+	if($_POST)
+	{
+		//get the post value from form
+		$numbers = $_POST['numbers'];
+		
+		if($numbers != ''){
+			//separate the numbers and make into array
+			$numArray = explode(',', $numbers);
+			
+			//now this array has both positive and negative integers
+			//let's separate it
+			foreach($numArray as $num){
+				if($num > 0){
+					$positiveArr[] = $num;
+				}else{
+					$negativeArr[] = $num;
+				}
+			}
+ 
+			print "Positive Values: ". print_r($positiveArr,true)."<br>";
+			print "Negative Values: ". print_r($negativeArr,true);
+		}
+	}
+ 
     ?>
 </body>
 </html>
