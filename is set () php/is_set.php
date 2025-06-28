6a5b7c29cb9b6fcabb73f451e71855f37c7896c2
hiclassic <biswasashikuzzaman@gmail.com>
2025-06-28 10:01:34 +0600

    Update is_set.php

---
+++ is set () php/is_set.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>isset()</title>
+</head>
+<body>
+    <?php 
+    $name="Ashikuzzaman Biswas";
+    if(isset($name)) {
+        echo "name is set"; // output name is set
+    } else {
+        echo "name is not set";
+    }
+    ?>
+    <br>
+    <?php 
+    $country=null;
+    if(isset($country)) {
+        echo "country is set!";
+    } else {
+        echo "country is not set!";
+    }
+    ?>
+    <br>
+    <?php
+    if(isset($districte)) {
+        echo "District is set!";
+    }
+    else {
+        echo "District is not set!";
+    }
+    ?>
+</body>
+</html>
