134ba5eeee84e3595dd187446f2458c9d11bd8e8
hiclassic <biswasashikuzzaman@gmail.com>
2025-06-29 10:08:35 +0600

    update

---
+++ trait/trait.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>trait</title>
+</head>
+<body>
+    <?php
+    trait message1 {
+public function msg1() {
+    echo "OOP is fun! ";
+  }
+}
+
+class Welcome {
+  use message1;
+}
+
+$obj = new Welcome();
+$obj->msg1();
+    ?>
+</body>
+</html>
