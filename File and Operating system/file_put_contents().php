4d3aecfed4aa978c518362b726a2213b790815f2
hiclassic <biswasashikuzzaman@gmail.com>
2025-07-07 09:58:43 +0600

    Update file_put_contents().php

--- File and Operating system/file_put_contents().php
+++ File and Operating system/file_put_contents().php
@@ -1,4 +1,7 @@
 <?php 
 echo file_put_contents("data.txt", "Welcome to PHP File Handling!");
-
-?>
+echo"<br";
+$data="this is a test data";
+echo $data;
+echo "<br>";
+print_r(explode(" ", $data ,2));
