e55660553fc22b97d40ce33c06cf7441e20d09b9
hiclassic <biswasashikuzzaman@gmail.com>
2025-07-09 11:28:52 +0600

    Update secure.php

--- Encrypt_Deccrypt/secure.php
+++ Encrypt_Deccrypt/secure.php
@@ -1,4 +1,39 @@
+<!-- PHP md5(), sha1(), hash() Functions,base64_encode,base64_decode -->
+
 <?php
-$data = "hello12356";
-$hash = md5($data);
-echo $hash; // Output: 79f1c4c38dbcb19cda054974dba7b3ef
+$password = "admin";
+//md5()
+$m = md5($password);
+echo $m;
+echo "<br>";
+echo strlen($m);
+
+echo "<br>";
+//sha1(),
+$m = sha1($password);
+echo $m;
+echo "<br>";
+echo strlen($m);
+
+
+echo "<br>";
+//hash()
+$m = hash('sha1', 'this is admin');
+echo $m;
+echo "<br>";
+echo strlen($m);
+
+
+
+echo "<br>";
+//base64_encode(),
+$m = base64_encode($password);
+echo $m;
+echo "<br>";
+echo strlen($m);
+
+
+echo "<br>";
+echo base64_decode("YWRtaW4=");
+
+?>
